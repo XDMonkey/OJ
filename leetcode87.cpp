b0a122fad11f54e14de0696d2d020abd62ea76da
yangkun i <yangkun i>
2017-02-17 12:31:49 +0800

    leetcode87

--- leetcode87.cpp
+++ leetcode87.cpp
@@ -1,47 +1,66 @@
-// leetcode87.cpp : 定义控制台应用程序的入口点。
+// leetcode76.cpp : 定义控制台应用程序的入口点。
 //
 
 #include "stdafx.h"
-#include <string>
-#include <iostream>
+#include<iostream>
+#include<map>
+#include<set>
+#include<string>
 using namespace std;
 class Solution {
 public:
-	bool isScramble(string s1, string s2) {
-		int length = s1.size();
-		int ***res = new int **[length];
-		for (int i = 0; i < length; i++) {
-			res[i] = new int*[length];
-			for (int j = 0; j < length; j++)
-				res[i][j] = new int[length];
+	string minWindow(string s, string t) {
+		map<char, int> res;
+		map<char, int> rec;
+		for (int i = 0; i < t.size(); i++) {
+			res[t[i]] ++;
+			rec[t[i]] = 0;
 		}
-		for (int i = 0; i < length; i++)
-			for (int j = 0; j < length; j++) {
-				if (s1[i] != s2[j])
-					res[0][i][j] = false;
-				else
-					res[0][i][j] = true;
-			}
-		for (int i = 0; i < length; i++) {
-			for (int j = 0; j <= length - 1 - i; j++) {
-				for (int m = 0; m <= length - 1 - i; m++)
-					for (int k = 0; k < i; k++) {
-						if ((res[k][j][m] && res[i - k - 1][j + k + 1][m + k + 1]) || (res[k][j][m + i - k] && res[i - k - 1][j + k + 1][m])) {
-							res[i][j][m] = true;
-							break;
-						}
-						res[i][j][m] = false;
+		int start_p = 0;
+		int end_p = 0;
+		int min = s.size() - 1;
+		int start = 0;
+		int end = 0;
+		int flag2 = 0;
+		while (end_p != s.size()) {
+			if (res.find(s[end_p]) != res.end()) {
+				rec[s[end_p]]++;
+				int flag = 1;
+				for (auto i = res.begin(), j = rec.begin(); i != res.end(); i++, j++) {
+					if (i->second > j->second) {
+						flag = 0;
+						break;
 					}
+				}
+				if (flag) {
+					flag2 = 1;
+					while (!(res.find(s[start_p]) != res.end() && res[s[start_p]] == rec[s[start_p]])) {
+						if (res.find(s[start_p]) != res.end())
+							rec[s[start_p]]--;
+						start_p++;
+					}
+					if (end_p - start_p <= min) {
+						min = end_p - start_p;
+						start = start_p;
+						end = end_p;
+					}
+					rec[s[start_p]]--;
+					start_p++;
+				}
 			}
+			end_p++;
 		}
-		return res[length - 1][0][0];
+		if (flag2)
+			return s.substr(start, end - start + 1);
+		return string("");
 	}
 };
+
+
 int main()
 {
 	Solution a;
-	if (a.isScramble("rgtae", "great"))
-		cout << "yes";
+	cout << a.minWindow("ADOBECODEBANC", "ABC");
 	return 0;
 }
 
